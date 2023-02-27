6392c6ee8e78b60c410e5c8f990bd5b732a1c225
LapisRaider <angielimenqi7@gmail.com>
2023-02-27 20:09:41 +0800

    Able to take photos from the camera

--- Assets/Scripts/PhotoTaking/PhotoCapture.cs
+++ Assets/Scripts/PhotoTaking/PhotoCapture.cs
@@ -1,15 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PhotoCapture : MonoBehaviour
 {
-    private Camera m_photoTakingCamera;
+    public RenderTexture m_photoTargetTexture;
+
+    private WaitForEndOfFrame m_enumeratorEndOfFrame = new WaitForEndOfFrame();
+    public Camera m_photoTakingCamera;
+    private Rect m_regionToRead;
+    private int m_currPhotoCount = 0;
+    private bool m_isTakingPhoto = false;
 
     // Start is called before the first frame update
     void Awake()
     {
-        m_photoTakingCamera = GetComponent<Camera>();
+        m_regionToRead = new Rect(0, 0, m_photoTargetTexture.width, m_photoTargetTexture.height);
+    }
+
+    private void OnEnable()
+    {
+        RenderPipelineManager.endContextRendering += RenderPipelineManager_endCameraRendering;
+    }
+
+    private void OnDisable()
+    {
+        RenderPipelineManager.endContextRendering -= RenderPipelineManager_endCameraRendering;
+    }
+
+    private void RenderPipelineManager_endCameraRendering(ScriptableRenderContext arg1, List<Camera> arg2)
+    {
+        if (!m_isTakingPhoto)
+            return;
+
+        m_isTakingPhoto = false;
+
+        Texture2D photoTaken = new Texture2D(m_photoTargetTexture.width, m_photoTargetTexture.height, TextureFormat.ARGB32, false);
+        m_regionToRead = new Rect(0, 0, m_photoTargetTexture.width, m_photoTargetTexture.height);
+        photoTaken.ReadPixels(m_regionToRead, 0, 0);
+        photoTaken.Apply();
+
+        byte[] byteArray = photoTaken.EncodeToPNG();
+        System.IO.File.WriteAllBytes(Application.dataPath + "/photos/photo" + m_currPhotoCount + ".png", byteArray);
+
+        ++m_currPhotoCount;
     }
 
     // Update is called once per frame
@@ -18,6 +52,7 @@ public class PhotoCapture : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             //take photo
+            m_isTakingPhoto = true;
         }
     }
 }
