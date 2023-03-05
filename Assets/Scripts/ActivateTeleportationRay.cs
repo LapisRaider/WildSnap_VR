55157ca975126a3f69f66c77285171902ec5e0ba
LapisRaider <angielimenqi7@gmail.com>
2023-03-06 01:27:22 +0800

    Able to interact and grab objects

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -8,15 +8,12 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty m_teleportActivation;
     public float m_teleportTriggerThreshold = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("Another action button used that is same as teleport activation button")]
+    public InputActionProperty m_cancelTeleportation;
 
     // Update is called once per frame
     void Update()
     {
-        m_teleportationRay.SetActive(m_teleportActivation.action.ReadValue<float>() > m_teleportTriggerThreshold);
+        m_teleportationRay.SetActive(m_cancelTeleportation.action.ReadValue<float>() == 0 && m_teleportActivation.action.ReadValue<float>() > m_teleportTriggerThreshold);
     }
 }
