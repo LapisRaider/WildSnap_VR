1633d43623e037b02bf99655c05f974672689f38
Lio Lim <65244648+WhiteLio@users.noreply.github.com>
2023-04-06 16:26:23 +0800

    Have UI follow player cam

--- Assets/Scripts/AnimalDex/UiManager.cs
+++ Assets/Scripts/AnimalDex/UiManager.cs
@@ -7,6 +7,7 @@ public class UiManager : MonoBehaviour
 {
     public InputActionReference m_inputToggleAnimalDex;
     public GameObject m_animalDex;
+    public Transform m_playerCamera;
 
     private bool isAnimalDexOpen;
 
@@ -19,7 +20,15 @@ public class UiManager : MonoBehaviour
     private void ToggleAnimalDex(InputAction.CallbackContext obj)
     {
         isAnimalDexOpen = !isAnimalDexOpen;
+        
+        if (isAnimalDexOpen && this.gameObject.activeInHierarchy)
+        {
+            transform.position = m_playerCamera.position;
+            Vector3 newEulerAngles = transform.eulerAngles;
+            newEulerAngles.y = m_playerCamera.eulerAngles.y;
+            transform.eulerAngles = newEulerAngles;
+        }
+
         m_animalDex.SetActive(isAnimalDexOpen);
-        // TODO make it face player
     }
 }
