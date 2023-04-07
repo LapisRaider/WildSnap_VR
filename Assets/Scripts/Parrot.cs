b36524d8f0c9692ca05282225f8596ce2a32648c
abner <abnermtj@live.com.sg>
2023-04-08 01:45:12 +0800

    add parot, also updated plugin to use newer animals

---
+++ Assets/Scripts/Parrot.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Parrot : MonoBehaviour
+{
+    public float MOVE_TIMEOUT = 5.0f;
+    public float flyAwayDistance = 5.0f;
+    public float avoidanceRadius = 3.0f;
+
+    private Transform playerTransform;
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Vector3 initialPos;
+    private Vector3 goalPos;
+    private bool isAscending; // Whether the bird is ascending or descending
+
+    private bool isRunningAway = false;
+    private bool hasFlown = false;
+    private float moveTimer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        isAscending = true; // Set bird to ascending initially
+        if (playerTransform == null)
+        {
+            Debug.LogError("Player not found! Make sure the player object has the 'Player' tag.");
+        }
+    }
+
+    void Update()
+    {
+        if (playerTransform == null)
+        {
+            return;
+        }
+
+        Vector3 distanceToPlayer = transform.position - playerTransform.position;
+
+        // If player is close
+        if (distanceToPlayer.magnitude < avoidanceRadius && !isRunningAway)
+        {
+            // Run away by setting goal position to be away from the player
+            isRunningAway = true;
+            hasFlown = false;
+            Vector3 moveDirection = distanceToPlayer.normalized * Random.Range(flyAwayDistance * 0.9f, flyAwayDistance * 1.1f);
+            initialPos = transform.localPosition;
+            goalPos = moveDirection + transform.localPosition;
+
+            agent.SetDestination(goalPos);
+            animator.SetBool("isFlying", true);
+        }
+
+        if (isRunningAway)
+        {
+            moveTimer += Time.deltaTime;
+            if (moveTimer > MOVE_TIMEOUT || (transform.localPosition - goalPos).magnitude < 0.3f)
+            {
+                moveTimer = 0f;
+                isRunningAway = false;
+                animator.SetBool("isFlying", false);
+            }
+        }
+    }
+}
