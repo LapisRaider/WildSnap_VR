053006437809e07d9034bad199b7a4420f15acec
LapisRaider <angielimenqi7@gmail.com>
2023-04-09 01:54:32 +0800

    Made it so you can teleport while holding objects and wont teleport while interacting with UI

--- Assets/Scripts/VrRelated/ActivateTeleportationRay.cs
+++ Assets/Scripts/VrRelated/ActivateTeleportationRay.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.UI;
+using UnityEngine.EventSystems;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
@@ -23,8 +26,11 @@ public class ActivateTeleportationRay : MonoBehaviour
          * 3. the interactable ray that shows up to interact with interactable obj is not shown
         */
 
-        bool isInteractableRayActive = m_interactableRay.TryGetHitInfo(out Vector3 pos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        //bool isInteractableRayActive = m_interactableRay.TryGetHitInfo(out Vector3 pos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        //bool isOtherActionInputActive = m_cancelTeleportation.action.ReadValue<float>() == 0;
+        bool isHittingUi = m_interactableRay.TryGetCurrentUIRaycastResult(out RaycastResult raycastResult, out int raycastEndpointIndex);
+        bool isPlayerHoldingTeleport = m_teleportActivation.action.ReadValue<float>() > m_teleportTriggerThreshold;
 
-        m_teleportationRay.SetActive(!isInteractableRayActive && m_cancelTeleportation.action.ReadValue<float>() == 0 && m_teleportActivation.action.ReadValue<float>() > m_teleportTriggerThreshold);
+        m_teleportationRay.SetActive(isPlayerHoldingTeleport && !isHittingUi);
     }
 }
