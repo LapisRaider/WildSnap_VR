1ddd2caf43ba098f1f1fc94456cd2a43c1fe204a
Jonas Chow <jonaschow99@gmail.com>
2023-04-10 01:02:49 +0800

    make apples respawn after eaten

--- Assets/Scripts/Animal/FoodManager.cs
+++ Assets/Scripts/Animal/FoodManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class FoodManager : MonoBehaviour
 {
+    public GameObject applePrefab;
+    public float appleSpawnCooldown;
+
     List<Transform> m_appleTransforms = new List<Transform>();
+    List<Vector3> m_initialPositions = new List<Vector3>();
+    List<Quaternion> m_initialRotations = new List<Quaternion>();
 
     public delegate void OnAppleEaten();
     public OnAppleEaten onAppleEatenCallback;
@@ -15,6 +20,8 @@ public class FoodManager : MonoBehaviour
         foreach (GameObject apple in apples)
         {
             m_appleTransforms.Add(apple.transform);
+            m_initialPositions.Add(apple.transform.position);
+            m_initialRotations.Add(apple.transform.rotation);
         }
     }
 
@@ -34,11 +41,30 @@ public class FoodManager : MonoBehaviour
 
     public void DestroyApple(AnimalDetection animalDetection)
     {
-        m_appleTransforms.Remove(animalDetection.m_currentAppleTransform);
+        int appleIndex = m_appleTransforms.IndexOf(animalDetection.m_currentAppleTransform);
+        if (appleIndex == -1) return;
+
+        Vector3 newApplePosition = m_initialPositions[appleIndex];
+        Quaternion newAppleRotation = m_initialRotations[appleIndex];
+        m_appleTransforms.RemoveAt(appleIndex);
+        m_initialPositions.RemoveAt(appleIndex);
+        m_initialRotations.RemoveAt(appleIndex);
         Destroy(animalDetection.m_currentAppleTransform.gameObject);
         animalDetection.m_currentAppleTransform = null;
 
+        StartCoroutine(CreateApple(newApplePosition, newAppleRotation));
+
         if (onAppleEatenCallback != null)
             onAppleEatenCallback.Invoke();
+    }
+
+    public IEnumerator CreateApple(Vector3 position, Quaternion rotation)
+    {
+        yield return new WaitForSeconds(appleSpawnCooldown);
+        
+        GameObject newApple = Instantiate(applePrefab, position, rotation);
+        m_appleTransforms.Add(newApple.transform);
+        m_initialPositions.Add(position);
+        m_initialRotations.Add(rotation);
     }
 }
